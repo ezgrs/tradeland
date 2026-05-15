0f9f5ecf23b02a151461abaf1969a2b28ee75d98
Enzo Santos <enzo.santos29@hotmail.com>
2026-05-15 15:52:17 -0300

    feat: remove ClasseGolpe do conceito de golpes

--- src/domain/data/golpes.ts
+++ src/domain/data/golpes.ts
@@ -1,32 +1,31 @@
-import { ClasseGolpe } from "../entities/Golpe"
 import { TipoPersonagem } from "../entities/Personagem"
 
-export const tiposGolpes: Record<
-    TipoPersonagem,
-    Record<ClasseGolpe, string>
-> = {
-    curandeiro: {
-        2: "claraoLuz",
-        4: "nevoaLacrimejante",
-        8: "rajadaFogo",
-        16: "penitencia",
-        32: "choqueSagrado",
-        64: "curaReversa",
-    },
-    gladiador: {
-        2: "socoParalisante",
-        4: "picadaAbelha",
-        8: "avalancheManual",
-        16: "golpeCauterizador",
-        32: "murroAflicao",
-        64: "apunhaladaMortal",
-    },
-    mago: {
-        2: "raioEnergia",
-        4: "espinhosMagicos",
-        8: "rajadaFogo",
-        16: "trovaoIncandescente",
-        32: "explosaoMistica",
-        64: "soproDragao",
-    },
-}
+export const tiposGolpes = {
+    curandeiro: [
+        "claraoLuz",
+        "nevoaLacrimejante",
+        "rajadaFogo",
+        "penitencia",
+        "choqueSagrado",
+        "curaReversa",
+    ],
+    gladiador: [
+        "socoParalisante",
+        "picadaAbelha",
+        "avalancheManual",
+        "golpeCauterizador",
+        "murroAflicao",
+        "apunhaladaMortal",
+    ],
+    mago: [
+        "raioEnergia",
+        "espinhosMagicos",
+        "rajadaFogo",
+        "trovaoIncandescente",
+        "explosaoMistica",
+        "soproDragao",
+    ],
+} as const satisfies Record<TipoPersonagem, string[]>
+
+export type TipoGolpe<T extends TipoPersonagem> =
+    (typeof tiposGolpes)[T][number]
--- src/domain/entities/Golpe.ts
+++
@@ -1,1 +0,0 @@
-export type ClasseGolpe = 2 | 4 | 8 | 16 | 32 | 64
